bbd51c77ca184869fd568ed3bdc92f9a1f360ab4
Anastasia <shvayka92@mail.ru>
2022-07-05 15:53:53 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Book book = new Book("Граф Монте-Кристо", "А.Дюма", 1844);
+        System.out.println("book.getBookName = " + book.getBookName());
+        System.out.println("book.getAuthorName = " + book.getAuthor());
+        System.out.println("book.getPublicYear = " + book.getPublicYear());
+
+        Book book1 = new Book("Ведьмак", "А. Сапковский", 1992);
+        System.out.println("book1.bookName = " + book1.getBookName());
+        System.out.println("book1.authorName = " + book1.getAuthor());
+        System.out.println("book1.publicYear = " + book1.getPublicYear());
+
+        Author author = new Author("Александр", "Дюма");
+        System.out.println("author.firsName = " + author.getFirsName());
+        System.out.println("author.lastName = " + author.getLastName());
+
+        Author author1 = new Author("Анджей", "Сапковский");
+        System.out.println("author1.firsName = " + author1.getFirsName());
+        System.out.println("author1.lastName = " + author1.getLastName());
+
+
+        book.setPublicYear(1846);
+        System.out.println("book.getPublicYear() = " + book.getPublicYear());
+
     }
+
 }
